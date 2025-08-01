4e57bcde4d886b182349e04aa4c3cd1971eb6d4c
Maxime L <maxime30labbe@gmail.com>
2025-08-01 10:26:41 +0200

    fix: Fixed inital values

--- constants/initial-types-value/initialCompany.tsx
+++ constants/initial-types-value/initialCompany.tsx
@@ -1,10 +1,10 @@
 export const initialCompany = {
-  name: "test",
-  city: "Tourcoing",
-  email: "test@test.com",
-  phone: "+3311101010",
-  SIRET: "12345678901234",
-  SIREN: "123456789",
-  domainId: "job1",
+  name: "",
+  city: "",
+  email: "",
+  phone: "",
+  SIRET: "",
+  SIREN: "",
+  domainId: "",
   OpentoReco: false,
 };
--- constants/initial-types-value/initialUser.tsx
+++ constants/initial-types-value/initialUser.tsx
@@ -1,8 +1,8 @@
 export const initialUser = {
-  firstName: "test",
-  lastName: "test",
-  email: "test@test.com",
-  password: "Test123/",
-  city: "Tourcoing",
-  phone: "+3311101010",
+  firstName: "",
+  lastName: "",
+  email: "",
+  password: "",
+  city: "",
+  phone: "",
 };
